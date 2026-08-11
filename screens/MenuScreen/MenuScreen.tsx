e5fcc460114fa7084739aeb76a871f48645d7c50
Flaze07 <christianseiji.cs@gmail.com>
2026-08-11 12:34:16 +0900

    fix: fixed there's an update message keeps appearing

--- screens/MenuScreen/MenuScreen.tsx
+++ screens/MenuScreen/MenuScreen.tsx
@@ -12,7 +12,7 @@ type NavProps = NativeStackNavigationProp<RootStackParamList, "menu">;
 
 export function MenuScreen() {
 
-  const currentVersionUrl = "https://github.com/Flaze07/tetrio-android/releases/tag/2.1";
+  const currentVersionUrl = "https://github.com/Flaze07/tetrio-android/releases/tag/2.3";
 
   const navigation = useNavigation<NavProps>();
   const [modalVisible, setModalVisible] = useState(false);
